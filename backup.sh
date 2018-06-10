3e899022c6d405540a97b53abd948bb68d83243e
SuguruOoki <suguru.oki.programming@gmail.com>
2018-06-10 17:19:17 +0900

    [修正][追加] 諸々追加

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# Atom
+apm star --installed;
+
+# Brew
+brew file update;
+brew file push;
--- setup.sh
+++ setup.sh
@@ -1,38 +1,53 @@
  #!/bin/bash
 
-DOT_FILES=(.zshrc .zprofile .zshenv .zpreztorc .vimrc)
-
-#上記のファイル群をdotfilesにシンボリックリンクを貼って処理する
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
 
 #Homebrewをinstall
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# TODO: brewfileをとりあえず使っているが、ツール的に検討を行っていないので、よければ、他のものを使用してみる
 # brew fileをinstall
-# brew fileのinstall先をdotfiles以下に設定 => zshrcなどでも設定を書いている
+brew install 
 # brew fileのリポジトリを設定
-# brew fileをpullする
-brew file pull
+brew set_repo git@github.com:SuguruOoki/brew-file-backup.git
 
 # brewfileの内容をinstall
 # brew caskの中身をinstall
+brew file install;
 
-brew file install
+# この辺は現在のPCでは入れていないが、cask管理下におきたいアプリ群
+# TODO: 次のPCに変わった時やこのsetupを走らせてcask管理下置かれた場合はここから外す。
+brew cask install google-chrome;
+brew cask install sequel-pro;
+brew cask install virtualbox;
+brew cask install docker;
+brew cask install slack;
+brew cask install iterm2;
+brew cask install sketch;
 
 # ここにzshを /etc/shell に追加するものを入れる
 # /usr/local/bin/zsh を最後に追記
-# TODO: 本当はここを自動化したい
-
-sudo vi /etc/shells
+# TODO: 本当はここを自動化したいが、権限上うまく行くかわからない。
+chsh -s /bin/zsh;
 
 # defaultのやつをzshに変更
-chsh -s /usr/local/bin/zsh
+chsh -s /usr/local/bin/zsh;
+
+# preztoのinstall
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto";
+setopt EXTENDED_GLOB;
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}";
+done
+
+# この辺りからzshとpreztoのシンボリックリンク
+# 上記のファイル群をdotfilesにシンボリックリンクを貼って処理する
+DOT_FILES=(.zshrc .zprofile .zshenv .zpreztorc .vimrc)
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file;
+done
 
 # apm stars --installed としていたファイルを取り込む
-apm stars --user SuguruOoki --install
-
+apm stars --user SuguruOoki --install;
 
